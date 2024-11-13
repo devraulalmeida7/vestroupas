701fb5eabb39b1529ce670cbc0bb35a9d02d269f
Ana leticya <ana.l.albuquerque@aluno.senai.br>
2024-11-13 15:04:17 -0300

    finalizado

---
+++ front/vest/src/components/AddProduct/AddProduct.tsx
@@ -0,0 +1,9 @@
+export default function addProduct() {
+    return (
+        <>
+            <main>
+                
+            </main>
+        </>
+    )
+}
--- front/vest/src/components/HomePage/HomePage.tsx
+++ front/vest/src/components/HomePage/HomePage.tsx
@@ -2,6 +2,19 @@ import styles from '../HomePage/styles.module.css'
 import logo from '../../assets/logo.svg'
 import search from '../../assets/fi-rr-search.svg'
 import banner from '../../assets/banner.png'
+import produto1 from '../../assets/produto1.png'
+import produto2 from '../../assets/produto2.png'
+import produto3 from '../../assets/produto3.png'
+import produto4 from '../../assets/produto4.png'
+import produto5 from '../../assets/produto5.png'
+import produto6 from '../../assets/produto6.png'
+
+
+
+
+import categoria1 from '../../assets/categoria1.png'
+import categoria2 from '../../assets/categoria2.png'
+import categoria3 from '../../assets/categoria3.png'
 
 export default function HomePage() {
     return (
@@ -15,11 +28,11 @@ export default function HomePage() {
                 </nav>
             
                 <div className={styles.logoContent}>
-                    <img src={logo} alt="Logo da Vest" />
+                    <img className={styles.logo} src={logo} alt="Logo da Vest" />
                 </div>
                 
                 <nav className={styles.menuAux}>
-                    <img className={styles.logo} src={search} alt="Barra de pesquisa" />
+                    {/* <img className={styles.logo} src={search} alt="Barra de pesquisa" /> */}
                 </nav>
 
             </header>
@@ -27,52 +40,62 @@ export default function HomePage() {
 
             <main>
                 <section className={styles.newAds}>
-                    <img src={banner} alt="Banner of new collection"></img>
+                    <article className={styles.adCollection}>
+                        <img src={banner} alt="Banner of new collection"></img>
+                    </article>
+
+                    <article className={styles.heading}>
+                        <h1 className={styles.headingMain}>New Collection</h1>
+                        <p className={styles.headingText}>As mais novas coleções, que combinam o clássico com o moderno em
+                        uma perfeita harmonia. </p>
+                    </article>
+                    
                 </section>
+
+
 
                 <section className={styles.newCollection}>
 
+                    <div className={styles.newCollectionContent}>
                     <article className={styles.produto}>
-                        <img src="" alt=""></img>
-                        <p></p>
-                        <p></p>
-                    </article>
+                            <img className={styles.produtoImage} src={produto1} alt="Camisa Branca 2024"></img>
+                            <p className={styles.title}>Camisa Branca 2024</p>
+                            <p>R$ 230,00</p>
+                        </article>
 
-                    <article className={styles.produto}>
-                        <img src="" alt=""></img>
-                        <p></p>
-                        <p></p>
-                    </article>
+                        <article className={styles.produto}>
+                            <img className={styles.produtoImage} src={produto2} alt="Vestido Summer 2024"></img>
+                            <p className={styles.title}>Vestido Summer 2024</p>
+                            <p>R$ 350,00</p>
+                        </article>
 
-                    <article className={styles.produto}>
-                        <img src="" alt=""></img>
-                        <p></p>
-                        <p></p>
-                    </article>
+                        <article className={styles.produto}>
+                            <img className={styles.produtoImage} src={produto3} alt="Camisa Longa Verão"></img>
+                            <p className={styles.title}>Camisa Longa Verão</p>
+                            <p>R$ 250,00</p>
+                        </article>
+                    </div>
+                        
+                    <div className={styles.newCollectionContent}>
+                            <article className={styles.produto}>
+                            <img className={styles.produtoImage} src={produto4} alt="Camisa Branca 2024"></img>
+                            <p className={styles.title}>Camisa Branca 2024</p>
+                            <p>R$ 230,00</p>
+                        </article>
 
-                    <article className={styles.produto}>
-                        <img src="" alt=""></img>
-                        <p></p>
-                        <p></p>
-                    </article>
+                        <article className={styles.produto}>
+                            <img className={styles.produtoImage} src={produto5} alt="Vestido Summer 2024"></img>
+                            <p className={styles.title}>Vestido Summer 2024</p>
+                            <p>R$ 350,00</p>
+                        </article>
 
-                    <article className={styles.produto}>
-                        <img src="" alt=""></img>
-                        <p></p>
-                        <p></p>
-                    </article>
-
-                    <article className={styles.produto}>
-                        <img src="" alt=""></img>
-                        <p></p>
-                        <p></p>
-                    </article>
-
-                    <article className={styles.produto}>
-                        <img src="" alt=""></img>
-                        <p></p>
-                        <p></p>
-                    </article>
+                        <article className={styles.produto}>
+                            <img className={styles.produtoImage} src={produto6} alt="Camisa Longa Verão"></img>
+                            <p className={styles.title}>Camisa Longa Verão</p>
+                            <p>R$ 250,00</p>
+                        </article>
+                    </div>
+                    
 
                 </section>
             </main>
@@ -80,22 +103,19 @@ export default function HomePage() {
 
             <section className={styles.category}>
 
-                <article className={styles.categoryName}>
-                    <img src="" alt="" className={styles.categoryImage}></img>
-                    <h2 className={styles.categoryTitle}></h2>
-                    <button className={styles.categoryButton}></button>
+                <article className={styles.categoryContent}>
+                    <img src={categoria1} alt="" className={styles.categoryImage}></img>
+                    <h2 className={styles.categoryTitle}>HOMEM</h2>
                 </article>
 
-                <article className={styles.categoryName}>
-                    <img src="" alt="" className={styles.categoryImage}></img>
-                    <h2 className={styles.categoryTitle}></h2>
-                    <button className={styles.categoryButton}></button>
+                <article className={styles.categoryContent}>
+                    <img src={categoria2} alt="" className={styles.categoryImage}></img>
+                    <h2 className={styles.categoryTitle}>MULHER</h2>
                 </article>
                 
-                <article className={styles.categoryName}>
-                    <img src="" alt="" className={styles.categoryImage}></img>
-                    <h2 className={styles.categoryTitle}></h2>
-                    <button className={styles.categoryButton}></button>
+                <article className={styles.categoryContent}>
+                    <img src={categoria3} alt="" className={styles.categoryImage}></img>
+                    <h2 className={styles.categoryTitle}>INFANTIL</h2>
                 </article>
 
 
